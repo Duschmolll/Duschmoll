2b85732bf542a553fc885908d9051df68231ca1c
Duschmoll <duschmoll@zaclys.net>
2022-01-31 19:13:35 +0100

    Starting the OpenWordle game

--- duschmoll/page/openwordle/index.php
+++ duschmoll/page/openwordle/index.php
@@ -1,5 +1,7 @@
 <?php include_once "../../include/header.php" ?>
-
+<style>
+    <?php include_once "res/css/openwordle.css"; ?>
+</style>
 
 <div class="main-post">
     <section id="post" style="width: 1600px;">
@@ -10,7 +12,84 @@
         <br>
 
         <section id="game">
-
+            <section id='userInput'>
+                <div class='letterClear' id='rowOneLetterOne'></div>
+                <div class='letterClear' id='rowOneLetterTwo'></div>
+                <div class='letterClear' id='rowOneLetterThree'></div>
+                <div class='letterClear' id='rowOneLetterFour'></div>
+                <div class='letterClear' id='rowOneLetterFive'></div>
+            </section>
+            <section id='userInput'>
+                <div class='letterClear' id='rowTwoLetterOne'>W</div>
+                <div class='letterClear' id='rowTwoLetterTwo'>O</div>
+                <div class='letterClear' id='rowTwoLetterThree'>R</div>
+                <div class='letterClear' id='rowTwoLetterFour'>D</div>
+                <div class='letterClear' id='rowTwoLetterFive'>S</div>
+            </section>
+            <section id='userInput'>
+                <div class='letterClear' id='rowThreeLetterOne'>W</div>
+                <div class='letterClear' id='rowThreeLetterTwo'>O</div>
+                <div class='letterClear' id='rowThreeLetterThree'>R</div>
+                <div class='letterClear' id='rowThreeLetterFour'>D</div>
+                <div class='letterClear' id='rowThreeLetterFive'>S</div>
+            </section>
+            <section id='userInput'>
+                <div class='letterClear' id='rowFourLetterOne'>W</div>
+                <div class='letterClear' id='rowFourLetterTwo'>O</div>
+                <div class='letterClear' id='rowFourLetterThree'>R</div>
+                <div class='letterClear' id='rowFourLetterFour'>D</div>
+                <div class='letterClear' id='rowFourLetterFive'>S</div>
+            </section>
+            <section id='userInput'>
+                <div class='letterClear' id='rowFiveLetterOne'>W</div>
+                <div class='letterClear' id='rowFiveLetterTwo'>O</div>
+                <div class='letterClear' id='rowFiveLetterThree'>R</div>
+                <div class='letterClear' id='rowFiveLetterFour'>D</div>
+                <div class='letterClear' id='rowFiveLetterFive'>S</div>
+            </section>
+            <section id='userInput'>
+                <div class='letterClear' id='rowSixLetterOne'>W</div>
+                <div class='letterClear' id='rowSixLetterTwo'>O</div>
+                <div class='letterClear' id='rowSixLetterThree'>R</div>
+                <div class='letterClear' id='rowSixLetterFour'>D</div>
+                <div class='letterClear' id='rowSixLetterFive'>S</div>
+            </section>
+            <section id='keyboard'>
+                <section id='rowOne'>
+                    <div class='keyboardLetter'>a</div>
+                    <div class='keyboardLetter'>z</div>
+                    <div class='keyboardLetter'>e</div>
+                    <div class='keyboardLetter'>r</div>
+                    <div class='keyboardLetter'>t</div>
+                    <div class='keyboardLetter'>y</div>
+                    <div class='keyboardLetter'>u</div>
+                    <div class='keyboardLetter'>i</div>
+                    <div class='keyboardLetter'>o</div>
+                    <div class='keyboardLetter'>p</div>
+                </section>
+                <section id='rowTwo'>
+                    <div class='keyboardLetter'>q</div>
+                    <div class='keyboardLetter'>s</div>
+                    <div class='keyboardLetter'>d</div>
+                    <div class='keyboardLetter'>f</div>
+                    <div class='keyboardLetter'>g</div>
+                    <div class='keyboardLetter'>h</div>
+                    <div class='keyboardLetter'>j</div>
+                    <div class='keyboardLetter'>k</div>
+                    <div class='keyboardLetter'>l</div>
+                    <div class='keyboardLetter'>m</div>
+                </section>
+                <section id='rowThree'>
+                    <div class='keyboardSpecialKey'>ENTER</div>
+                    <div class='keyboardLetter'>w</div>
+                    <div class='keyboardLetter'>x</div>
+                    <div class='keyboardLetter'>c</div>
+                    <div class='keyboardLetter'>v</div>
+                    <div class='keyboardLetter'>b</div>
+                    <div class='keyboardLetter'>n</div>
+                    <div class='keyboardSpecialKey'>DEL</div>
+                </section>
+            </section>
         </section>
 
     </section>
---
+++ duschmoll/page/openwordle/js/openwordle.js
@@ -0,0 +1,34 @@
+const keyboardKeyPos = new Array('rowOneLetterOne', 'rowOneLetterTwo', 'rowOneLetterThree', 'rowOneLetterFour', 'rowOneLetterFive');
+let keyPos = 0;
+let answer = 'WORDS';
+
+//Listening to the keyboard input
+document.addEventListener('keydown', function (event) {
+
+    if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos < 5) { //Checking the input is key a of the alphabet.
+        document.getElementById(keyboardKeyPos[keyPos]).innerHTML = event.key.toUpperCase(); //Putting the user letter into his box pos.
+        keyPos++; //Passing to the next box pos.
+    } else if (event.key.match('Backspace') && keyPos > 0) { //Checking the input is Backspace.
+        document.getElementById(keyboardKeyPos[keyPos - 1]).innerHTML = ''; // Removing a user letter.
+        keyPos--; //Getting to the previous box pos.
+    } else if (event.key.match('Enter')) { //Checking the input is Enter.
+
+        compareAnswer();
+    }
+});
+
+//Function to compare the User input with the answer.
+function compareAnswer() {
+    console.log(answer.includes(document.getElementById(keyboardKeyPos[1]).innerHTML) === true);
+    for (let k = 0; k < keyboardKeyPos.length; k++) { //Loop to check all the letters to the answer letters.
+        elem = document.getElementById(keyboardKeyPos[k])
+        if (elem.innerHTML === answer[k]) { //Checking if the userAnswer[x] is equal the answer[x]. 
+            elem.setAttribute('Class', 'letterRight');
+        } else if (answer.includes(elem.innerHTML) === true) { //Checking if the userAnswer[x] is somewhere in the answer[x].
+            elem.setAttribute('Class', 'letterWrongPlace');
+        }
+        if (userAnswer === answer) {
+
+        }
+    }
+}
