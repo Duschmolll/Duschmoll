1d7dc95e19065b96516f9753a70308d1429d269b
Duschmoll <duschmoll@zaclys.net>
2022-02-07 16:03:04 +0100

    Finished the compare word to find & user's.

--- duschmoll/page/openwordle/js/openwordle.js
+++ duschmoll/page/openwordle/js/openwordle.js
@@ -11,11 +11,12 @@ let wordNumber = 0;
 let n = 0;
 let wordList = null;
 let wordListRefined = null;
-let answer = null;
-let word = {};
-let test = "blere";
-let userAnswer = '';
-let wordUser = {};
+let wordToFind = null;
+let wordToFindObject = {};
+let wordUserGuessed = '';
+let wordUserGuessedObject = {};
+
+
 //Fecthing the wordlist.
 fetch("js/index.json")
     .then(response => response.json())
@@ -27,15 +28,18 @@ fetch("js/index.json")
 
 //Function to refine the wordlist.
 function refiningWordList() {
+    //Settings for refining the word list.
     wordListRefined = wordList.filter(el => {
         return (
             el != null
             && el != ''
             && el.startsWith("w") === false
-            && el.length >= 6 && el.length < 7
+            && el.length >= 6 && el.length < 9
             && el.includes('-') === false
         );
     });
+
+    //Replacing the special character by some normal letters.
     for (let k = 0; k < wordListRefined.length; k++) {
         wordListRefined[k] = wordListRefined[k].replace(/['é','è','ê','ë']/g, 'e');
         wordListRefined[k] = wordListRefined[k].replace(/['à','â']/g, 'a');
@@ -45,10 +49,7 @@ function refiningWordList() {
         wordListRefined[k] = wordListRefined[k].replace(/['ç']/g, 'c');
         wordListRefined[k] = wordListRefined[k].replace(/['ü','ù','û']/g, 'u');
     }
-    console.log('Wordlist :')
-    console.log(wordList);
-    console.log('Wordlist Refined:')
-    console.log(wordListRefined);
+
     startGame();
 }
 
@@ -63,8 +64,9 @@ function startGame() {
         };
     };
 
+    //Picking the word to find.
     wordNumber = Math.floor(Math.random() * wordListRefined.length);
-    answer = wordListRefined[wordNumber].toLowerCase();
+    wordToFind = wordListRefined[wordNumber].toLowerCase();
 
     //Creating the letter of the word.
     const parentOfLetters = document.getElementById('letterGrid');
@@ -72,7 +74,7 @@ function startGame() {
         let sectionOfRow = document.createElement('section');
         sectionOfRow.id = 'userInput';
         parentOfLetters.appendChild(sectionOfRow);
-        for (let k = 0; k < answer.length; k++) {
+        for (let k = 0; k < wordToFind.length; k++) {
             let divOfLetter = document.createElement('div');
             divOfLetter.id = 'row' + x + 'Letter' + k;
             divOfLetter.classList.add('letterClear');
@@ -82,91 +84,12 @@ function startGame() {
 
     //Loop to launch put the first letter of the word to find at each row.
     for (let k = 0; k < 6; k++) {
-        document.getElementById('row' + k + 'Letter0').innerHTML = answer[0].toUpperCase(); //Putting the user letter into his box pos.
+        document.getElementById('row' + k + 'Letter0').innerHTML = wordToFind[0].toUpperCase(); //Putting the user letter into his box pos.
     }
-    userAnswer = answer[0];
-
-
-    for (let k = 0; k < answer.length; k++) {
-        word['letter' + k] = {};
-        word['letter' + k]['letter'] = answer[k];
-        word['letter' + k]['number'] = 0;
-        word['letter' + k]['found'] = false;
-
-    }
-    for (let k = 0; k < answer.length; k++) {
-        for (let x = 0; x < answer.length; x++) {
-            if (word['letter' + k]['letter'] === word['letter' + x]['letter']) {
-                word['letter' + k]['number'] = (word['letter' + k]['number'] + 1);
-            }
-        }
-    }
+    //Setting up the first letter into the user guessed input.
+    wordUserGuessed = wordToFind[0];
 };
 
-
-
-function compareAnswerTest() {
-    for (let k = 0; k < userAnswer.length; k++) {
-        wordUser['letter' + k] = {};
-        wordUser['letter' + k]['letter'] = userAnswer[k];
-        wordUser['letter' + k]['number'] = 0;
-        wordUser['letter' + k]['checked'] = false;
-
-    }
-    for (let k = 0; k < userAnswer.length; k++) {
-        for (let x = 0; x < userAnswer.length; x++) {
-            if (wordUser['letter' + k]['letter'] === wordUser['letter' + x]['letter']) {
-                wordUser['letter' + k]['number'] = (wordUser['letter' + k]['number'] + 1);
-            }
-        }
-    }
-
-    console.log('Compare is lauched.')
-    // The letter is right.
-    for (let k = 0; k < userAnswer.length; k++) {
-        if (word['letter' + k]['letter'] === wordUser['letter' + k]['letter'] && wordUser['letter' + k]['checked'] === false) {
-            console.log('Letter n°' + k + ' is at the right place.')
-            word['letter' + k]['found'] = true;
-            wordUser['letter' + k]['checked'] = true;
-            document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterRight');
-        }
-    }
-    // The letter is placed wrongly.
-    for (let k = 0; k < userAnswer.length; k++) {
-        for (let x = 0; x < userAnswer.length; x++) {
-            if (word['letter' + x]['letter'] === wordUser['letter' + k]['letter'] //check if the letter = the letter of the answer.
-                // && word['letter' + x]['number'] > 1 //chekc if it's a double letter
-                && word['letter' + x]['found'] === false //check if the letter is not already found.
-                && wordUser['letter' + k]['checked'] === false) { //check if the letter of the user is not already found.
-                console.log('Letter n°' + k + ' is at the wrong place and is the second or more letter of the same type.')
-                wordUser['letter' + k]['checked'] = true;
-                document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterWrongPlace');
-
-            }
-        }
-    }
-    //The letter is not present.
-    for (let k = 0; k < userAnswer.length; k++) {
-        if (wordUser['letter' + k]['checked'] === false) {
-            console.log('Letter n°' + k + ' is not present in the word.')
-            wordUser['letter' + k]['checked'] = true;
-        }
-    }
-    // let testplusplus = 0
-    // for (let k = 0; k < userAnswer.length; k++) {
-    //     if (word['letter' + k]['letter'] === 'e') {
-    //         testplusplus++;
-    //     }
-    // }
-    // console.log(testplusplus)
-
-    // function count(str, find) {
-    //     return (str.split(find)).length - 1;
-    // }
-
-    // count(answer, "e"); // 2
-
-}
 //Listening to the keyboard input & calling the function to check the input.
 const keyCheckConst = (event) => { keyCheck(event) };
 document.addEventListener('keydown', keyCheckConst);
@@ -180,22 +103,18 @@ function virtualKeybord(event) {
 //Function to check the user key pressed.
 function keyCheck(event) {
 
-    if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos < answer.length) { //Checking the input is key a of the alphabet.
+    //Checking the input is key a of the alphabet.
+    if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos < wordToFind.length) {
         document.getElementById('row' + rowPos + 'Letter' + keyPos).innerHTML = event.key.toUpperCase(); //Putting the user letter into his box pos.
-        userAnswer += event.key;
+        wordUserGuessed += event.key; //Adding the input into the User Guess.
         keyPos++; //Passing to the next box pos.
-        console.log(userAnswer)
     } else if (event.key.match('Backspace') && keyPos > 1) { //Checking the input is Backspace.
         document.getElementById('row' + rowPos + 'Letter' + (keyPos - 1)).innerHTML = ''; // Removing a user letter.
-        userAnswer = userAnswer.slice(0, userAnswer.length - 1);
+        wordUserGuessed = wordUserGuessed.slice(0, wordUserGuessed.length - 1); //Removing the input into the user Guess.
         keyPos--; //Getting to the previous box pos.
-        console.log(userAnswer)
-    } else if (event.key.match('Enter') && keyPos == answer.length) { //Checking the input is Enter and that the user as entered a long enough word.
-        if (wordListRefined.includes(userAnswer) === true) {
+    } else if (event.key.match('Enter') && keyPos == wordToFind.length) { //Checking the input is Enter and that the user as entered a long enough word.
+        if (wordListRefined.includes(wordUserGuessed) === true) {
             compareAnswerTest();
-            keyPos = 1;
-            rowPos++;
-            userAnswer = answer[0];
         } else {
             alert('Ce mot n\'est pas présent dans le dictionnaire.')
         }
@@ -203,23 +122,62 @@ function keyCheck(event) {
     }
 }
 
-// //Function to compare the User input with the answer.
-// function compareAnswer() {
-//     for (let k = 0; k < answer.length; k++) { //Loop to check all the letters to the answer letters.
+//Function to compare the User guess to the word to find.
+function compareAnswerTest() {
+    //Making the word to find into a decomposed object.
+    for (let k = 0; k < wordToFind.length; k++) {
+        wordToFindObject['letter' + k] = {};
+        wordToFindObject['letter' + k]['letter'] = wordToFind[k];
+        wordToFindObject['letter' + k]['found'] = false;
 
-//         elem = document.getElementById('row' + rowPos + 'Letter' + k);
-//         // userAnswer += elem.innerHTML;
-//         if (elem.innerHTML.toLowerCase() === answer[k]) { //Checking if the userAnswer[x] is equal the answer[x].
-//             console.log('Letter is right place')
-//             elem.setAttribute('Class', 'letterRight');
-//         } else if (answer.includes(elem.innerHTML.toLowerCase()) === true) { //Checking if the userAnswer[x] is somewhere in the answer[x].
-//             console.log('Letter is there but not place')
-//             elem.setAttribute('Class', 'letterWrongPlace');
-//         };
-//         if (userAnswer.toLowerCase() === answer || rowPos === 6) { //Removing the event to listen to the keys
-//             document.removeEventListener('keydown', keyCheckConst);
-//         };
-//     };
-// };
+    }
+
+    //Making the user guess word into a decomposed object.
+    for (let k = 0; k < wordUserGuessed.length; k++) {
+        wordUserGuessedObject['letter' + k] = {};
+        wordUserGuessedObject['letter' + k]['letter'] = wordUserGuessed[k];
+        wordUserGuessedObject['letter' + k]['checked'] = false;
+
+    }
+
+    // if the letter is right.
+    for (let k = 0; k < wordUserGuessed.length; k++) {
+        if (wordToFindObject['letter' + k]['letter'] === wordUserGuessedObject['letter' + k]['letter']
+            && wordUserGuessedObject['letter' + k]['checked'] === false) {
+            wordToFindObject['letter' + k]['found'] = true; //Making the letter of the word as found.
+            wordUserGuessedObject['letter' + k]['checked'] = true; //Making the letter of the user as checked.
+            document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterRight'); //Changing the color of the letter.
+        }
+    }
+
+    // if the letter is placed wrongly.
+    for (let k = 0; k < wordUserGuessed.length; k++) {
+        for (let x = 0; x < wordUserGuessed.length; x++) {
+            if (wordToFindObject['letter' + x]['letter'] === wordUserGuessedObject['letter' + k]['letter'] //check if the letter = the letter of the wordToFind.
+                && wordToFindObject['letter' + x]['found'] === false //check if the letter is not already found.
+                && wordUserGuessedObject['letter' + k]['checked'] === false) { //check if the letter of the user is not already found.
+                wordUserGuessedObject['letter' + k]['checked'] = true; //Making the letter of the user as checked.
+                wordToFindObject['letter' + x]['found'] = true; //Making the letter of the word as found.
+                document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterWrongPlace');//Changing the color of the letter.
+
+            }
+        }
+    }
+
+    //if the letter is not present.
+    for (let k = 0; k < wordUserGuessed.length; k++) {
+        if (wordUserGuessedObject['letter' + k]['checked'] === false) {
+            wordUserGuessedObject['letter' + k]['checked'] = true;
+        }
+    }
+
+    keyPos = 1;
+    rowPos++;
+    //Removing the event to listen to the keys.
+    if (wordUserGuessed === wordToFind || rowPos === 6) {
+        document.removeEventListener('keydown', keyCheckConst);
+    };
+    wordUserGuessed = wordToFind[0];
+}
 
 
