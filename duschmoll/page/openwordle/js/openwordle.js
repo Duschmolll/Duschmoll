21f537bcffc88193a4d0e9fe22715c68d1490e70
Duschmoll <duschmoll@zaclys.net>
2022-02-14 16:57:27 +0100

    Added Design && Double Start Letter

--- duschmoll/page/openwordle/js/openwordle.js
+++ duschmoll/page/openwordle/js/openwordle.js
@@ -15,7 +15,7 @@ let wordToFind = null;
 let wordToFindObject = {};
 let wordUserGuessed = '';
 let wordUserGuessedObject = {};
-
+let keyPressedTwice = false;
 
 //Fecthing the wordlist.
 fetch("js/index.json")
@@ -72,7 +72,7 @@ function startGame() {
     const parentOfLetters = document.getElementById('letterGrid');
     for (let x = 0; x < 6; x++) {
         let sectionOfRow = document.createElement('section');
-        sectionOfRow.id = 'userInput';
+        sectionOfRow.id = 'wordRow';
         parentOfLetters.appendChild(sectionOfRow);
         for (let k = 0; k < wordToFind.length; k++) {
             let divOfLetter = document.createElement('div');
@@ -87,9 +87,15 @@ function startGame() {
     //Loop to launch put the first letter of the word to find at each row.
     for (let k = 0; k < 6; k++) {
         document.getElementById('row' + k + 'Letter0').innerHTML = wordToFind[0].toUpperCase(); //Putting the user letter into his box pos.
+        document.getElementById('row' + k + 'Letter0').className = "letterPending"; //Changing the class to pending.
     }
+
     //Setting up the first letter into the user guessed input.
     wordUserGuessed = wordToFind[0];
+
+    //Changing the keyboard letter of the first later being already found.
+    document.getElementById(wordToFind[0]).setAttribute('Class', 'keyboardLetterRight');//Changing the color of the keyboard letter.
+
 };
 
 //Listening to the keyboard input & calling the function to check the input.
@@ -111,7 +117,7 @@ function virtualKeyboardCreation() {
                     let divOfKey = document.createElement('div');
                     divOfKey.id = azerty[k];
                     divOfKey.classList.add('keyboardLetter');
-                    divOfKey.innerHTML = azerty[k]
+                    divOfKey.innerHTML = azerty[k].toUpperCase()
                     divOfKey.setAttribute('onclick', 'virtualKeyboard(this)');
                     sectionOfRow0.appendChild(divOfKey);
                 };
@@ -124,7 +130,7 @@ function virtualKeyboardCreation() {
                     let divOfKey = document.createElement('div');
                     divOfKey.id = azerty[k];
                     divOfKey.classList.add('keyboardLetter');
-                    divOfKey.innerHTML = azerty[k]
+                    divOfKey.innerHTML = azerty[k].toUpperCase()
                     divOfKey.setAttribute('onclick', 'virtualKeyboard(this)');
                     sectionOfRow1.appendChild(divOfKey);
                 };
@@ -138,20 +144,20 @@ function virtualKeyboardCreation() {
                     let divOfKey = document.createElement('div');
                     divOfKey.id = azerty[k];
                     divOfKey.classList.add('keyboardLetter');
-                    divOfKey.innerHTML = azerty[k]
+                    divOfKey.innerHTML = azerty[k].toUpperCase()
                     divOfKey.setAttribute('onclick', 'virtualKeyboard(this)');
                     sectionOfRow2.appendChild(divOfKey);
                 };
                 let divOfKeySpecial1 = document.createElement('div');
                 divOfKeySpecial1.id = 'Backspace';
-                divOfKeySpecial1.classList.add('keyboardSpecialKey');
-                divOfKeySpecial1.innerHTML = 'Del'
+                divOfKeySpecial1.classList.add('keyboardSpecialKeyBackspace');
+                divOfKeySpecial1.innerHTML = 'DEL'
                 divOfKeySpecial1.setAttribute('onclick', 'virtualKeyboard(this)');
                 sectionOfRow2.appendChild(divOfKeySpecial1);
                 let divOfKeySpecial0 = document.createElement('div');
                 divOfKeySpecial0.id = 'Enter';
-                divOfKeySpecial0.classList.add('keyboardSpecialKey');
-                divOfKeySpecial0.innerHTML = 'Enter'
+                divOfKeySpecial0.classList.add('keyboardSpecialKeyEnter');
+                divOfKeySpecial0.innerHTML = 'ENTER'
                 divOfKeySpecial0.setAttribute('onclick', 'virtualKeyboard(this)');
                 sectionOfRow2.appendChild(divOfKeySpecial0);
                 break;
@@ -169,16 +175,23 @@ function virtualKeyboard(event) {
 function keyCheck(event) {
     console.log(event.key)
     //Checking the input is key a of the alphabet.
-    if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos < wordToFind.length) {
+    if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos === 1 && wordToFind[0].includes(event.key) === true && keyPressedTwice == false) { //Skipping the first letter is identical.
+        keyPressedTwice = true; //Setting up the skip of the if the letter start with a double letter.
+    }
+    else if (event.key.match(/[a-z]/) && event.key.length === 1 && keyPos < wordToFind.length) {
         document.getElementById('row' + rowPos + 'Letter' + keyPos).innerHTML = event.key.toUpperCase(); //Putting the user letter into his box pos.
+        document.getElementById('row' + rowPos + 'Letter' + keyPos).className = "letterPending"; //Changing the class to pending.
+        keyPressedTwice = false; //Resetting the skip of the double letter
         wordUserGuessed += event.key; //Adding the input into the User Guess.
         keyPos++; //Passing to the next box pos.
     } else if (event.key.match('Backspace') && keyPos > 1) { //Checking the input is Backspace.
         document.getElementById('row' + rowPos + 'Letter' + (keyPos - 1)).innerHTML = ''; // Removing a user letter.
+        document.getElementById('row' + rowPos + 'Letter' + (keyPos - 1)).className = "letterClear"; //Changing the class to pending.
         wordUserGuessed = wordUserGuessed.slice(0, wordUserGuessed.length - 1); //Removing the input into the user Guess.
+
         keyPos--; //Getting to the previous box pos.
     } else if (event.key.match('Enter') && keyPos == wordToFind.length) { //Checking the input is Enter and that the user as entered a long enough word.
-        if (wordListRefined.includes(wordUserGuessed) === true) {
+        if (wordListRefined.includes(wordUserGuessed) === false) {
             compareAnswerTest();
         } else {
             alert('Ce mot n\'est pas présent dans le dictionnaire.')
@@ -212,6 +225,7 @@ function compareAnswerTest() {
             wordToFindObject['letter' + k]['found'] = true; //Making the letter of the word as found.
             wordUserGuessedObject['letter' + k]['checked'] = true; //Making the letter of the user as checked.
             document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterRight'); //Changing the color of the letter.
+            document.getElementById(wordUserGuessedObject['letter' + k]['letter']).setAttribute('Class', 'keyboardLetterRight');//Changing the color of the keyboard letter.
         }
     }
 
@@ -224,7 +238,7 @@ function compareAnswerTest() {
                 wordUserGuessedObject['letter' + k]['checked'] = true; //Making the letter of the user as checked.
                 wordToFindObject['letter' + x]['found'] = true; //Making the letter of the word as found.
                 document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterWrongPlace');//Changing the color of the letter.
-
+                document.getElementById(wordUserGuessedObject['letter' + k]['letter']).setAttribute('Class', 'keyboardLetterWrongPlace');//Changing the color of the keyboard letter.
             }
         }
     }
@@ -233,9 +247,14 @@ function compareAnswerTest() {
     for (let k = 0; k < wordUserGuessed.length; k++) {
         if (wordUserGuessedObject['letter' + k]['checked'] === false) {
             wordUserGuessedObject['letter' + k]['checked'] = true;
+            document.getElementById('row' + rowPos + 'Letter' + k).setAttribute('Class', 'letterNotPresent');//Changing the color of the letter.
         }
     }
-
+    for (let k = 0; k < wordUserGuessed.length; k++) {
+        if (wordToFind.includes(wordUserGuessedObject['letter' + k]['letter']) === false) {
+            document.getElementById(wordUserGuessedObject['letter' + k]['letter']).setAttribute('Class', 'keyboardLetterNotPresent');//Changing the color of the keyboard letter.
+        }
+    }
     keyPos = 1;
     rowPos++;
     //Removing the event to listen to the keys.
